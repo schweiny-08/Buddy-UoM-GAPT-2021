3d867ecc2c4c5940d0a0dea392d11d9ccb2f2a24
Nina Musumeci <nina.musumeci.19@um.edu.mt>
2021-04-20 11:03:16 +0200

    Models Created

---
+++ BuddyAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class User
+{
+	public User()
+	{
+		public int userId { get; set; }
+		public string username { get; set; }
+		public int telephone { get; set; }
+		public string email { get; set; }
+		public string password { get; set; }
+
+		public virtual ICollection<User> user { get; set; }
+	}
+}
---
+++ BuddyAPI/Models/UserRoles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class UserRoles
+{
+	public UserRoles()
+	{
+		public int userId { get; set; }
+		public int roleId { get; set; }
+
+		public virtual ICollection<UserRoles> userRole { get; set; }
+	}
+}
