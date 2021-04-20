3168f4a48246910a88dc45ee13028c2de589512b
schweiny-08 <andrew.magri@outlook.com>
2021-04-20 20:05:33 +0200

    Index file now shows up
    VERY IMP:
    I HAVE EXCLUDED MODELS FOLDER

--- BuddyAPI/Startup.cs
+++ BuddyAPI/Startup.cs
@@ -4,11 +4,13 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -43,6 +45,12 @@ namespace BuddyAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BuddyAPI v1"));
             }
+
+            app.UseFileServer(new FileServerOptions { 
+                FileProvider = new PhysicalFileProvider(
+                        Path.Combine(Directory.GetCurrentDirectory(), "StaticFile")),
+                RequestPath = "/StaticFile"
+            });
 
             app.UseHttpsRedirection();
 
