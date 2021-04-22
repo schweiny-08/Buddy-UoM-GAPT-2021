7e50b5821fd098bfa63fead95bbeeb1d1cacaa5c
stefsche <58699420+stefsche@users.noreply.github.com>
2021-04-22 23:49:34 +0200

    Added Level -1 Floor Plans and custom level selector
    -Added a floor plan for Faculty of ICT Level -1
    -Added radio buttons and necessary js code to select which level to render regardless of zoom level.
    -User can opt to no show any overlay
    -Slightly styled the map and radio buttons to be centred
    -Currently Level 0 shows mock floor plans

--- BuddyAPI/wwwroot/js/ictMap.js
+++ BuddyAPI/wwwroot/js/ictMap.js
@@ -6,21 +6,44 @@ function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         center: ict,
         zoom: 15,
-    });    
+    });
     const coordInfoWindow = new google.maps.InfoWindow();
     coordInfoWindow.setContent(createInfoWindowContent(ict, map.getZoom()));
     coordInfoWindow.setPosition(ict);
-    coordInfoWindow.open(map);
+    //coordInfoWindow.open(map);
     //event listener that fires when zoom level changes
     map.addListener("zoom_changed", () => {
         coordInfoWindow.setContent(
             createInfoWindowContent(ict, map.getZoom())
         );
-        coordInfoWindow.open(map);
+        //coordInfoWindow.open(map);
     });
     //setting the bounds of UOM. Bounds [zoom][coord.x][coord.y]
     //bounds are generated from map tiler folder structure when tiles are created
     const bounds = {
+        18: [
+            [141619, 141620],
+            [103028, 103028],
+        ],
+        19: [
+            [283239, 283240],
+            [206056, 206057],
+        ],
+        20: [
+            [566478, 566480],
+            [412112, 412115],
+        ],
+        21: [
+            [1132956, 1132960],
+            [824225, 824231],
+        ],
+        22: [
+            [2265913, 2265920],
+            [1648450, 1648462],
+        ],
+    };
+
+    const bounds2 = {
         19: [
             [283239, 283240],
             [206056, 206057],
@@ -37,16 +60,48 @@ function initMap() {
 
     //https://developers.google.com/maps/documentation/javascript/examples/maptype-image-overlay#maps_maptype_image_overlay-javascript
     //creating the imageMapType for the image to be overlayed over the map
-    const blkA = new google.maps.ImageMapType({
+
+
+    const ictLvlMinOne = new google.maps.ImageMapType({
+        getTileUrl: function (coord, zoom) {
+            //if statement to not render the image if outside the bounds or outside the max/min zoom levels
+            if (
+                zoom < 18 ||
+                zoom > 22 ||
+                bounds[zoom][0][0] > coord.x ||
+                coord.x > bounds[zoom][0][1] ||
+                bounds[zoom][1][0] > coord.y ||
+                coord.y > bounds[zoom][1][1]
+            ) {
+                return "";
+            }
+            //returning the required tiles by requesting the specific png images
+            //link is according to the port being used 
+            return [
+                "https://localhost:",
+                port,
+                "/images/Lvl-1/",
+                zoom,
+                "/",
+                coord.x,
+                "/",
+                coord.y,
+                ".png",
+            ].join("");
+        },
+        tileSize: new google.maps.Size(256, 256),
+    });
+
+    const ictLvlZeroBlkA = new google.maps.ImageMapType({
         getTileUrl: function (coord, zoom) {
             //if statement to not render the image if outside the bounds or outside the max/min zoom levels
             if (
                 zoom < 19 ||
                 zoom > 21 ||
-                bounds[zoom][0][0] > coord.x ||
-                coord.x > bounds[zoom][0][1] ||
-                bounds[zoom][1][0] > coord.y ||
-                coord.y > bounds[zoom][1][1]
+                bounds2[zoom][0][0] > coord.x ||
+                coord.x > bounds2[zoom][0][1] ||
+                bounds2[zoom][1][0] > coord.y ||
+                coord.y > bounds2[zoom][1][1]
             ) {
                 return "";
             }
@@ -66,16 +121,17 @@ function initMap() {
         },
         tileSize: new google.maps.Size(256, 256),
     });
-    const blkB = new google.maps.ImageMapType({
+
+    const ictLvlZeroBlkB = new google.maps.ImageMapType({
         getTileUrl: function (coord, zoom) {
             //if statement to not render the image if outside the bounds or outside the max/min zoom levels
             if (
                 zoom < 19 ||
                 zoom > 21 ||
-                bounds[zoom][0][0] > coord.x ||
-                coord.x > bounds[zoom][0][1] ||
-                bounds[zoom][1][0] > coord.y ||
-                coord.y > bounds[zoom][1][1]
+                bounds2[zoom][0][0] > coord.x ||
+                coord.x > bounds2[zoom][0][1] ||
+                bounds2[zoom][1][0] > coord.y ||
+                coord.y > bounds2[zoom][1][1]
             ) {
                 return "";
             }
@@ -95,6 +151,25 @@ function initMap() {
         },
         tileSize: new google.maps.Size(256, 256),
     });
-    map.overlayMapTypes.push(blkA);
-    map.overlayMapTypes.push(blkB);
+
+    var radio = document.forms["level-selector"].elements["level"];
+    for (var i = 0; i < radio.length; i++) {
+        radio[i].onclick = function () {
+            if (document.getElementById('level -1').checked) {
+                map.overlayMapTypes.clear();
+                map.overlayMapTypes.push(ictLvlMinOne);
+                document.getElementById('status').innerHTML = "Level -1";
+            }
+            if (document.getElementById('level 0').checked) {
+                map.overlayMapTypes.clear();
+                map.overlayMapTypes.push(ictLvlZeroBlkA);
+                map.overlayMapTypes.push(ictLvlZeroBlkB);
+                document.getElementById('status').innerHTML = "Level 0";
+            }
+            if (document.getElementById('no-overlay').checked) {
+                map.overlayMapTypes.clear();
+                document.getElementById('status').innerHTML = "No Overlay";
+            }
+        }
+    }
 }
