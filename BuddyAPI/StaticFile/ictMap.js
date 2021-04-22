a2672df1494ebe87f05ae9712171d6a832367050
stefsche <58699420+stefsche@users.noreply.github.com>
2021-04-22 12:23:50 +0200

    Modified Map for ICT building and separated functions in external files

---
+++ BuddyAPI/StaticFile/ictMap.js
@@ -0,0 +1,83 @@
+﻿//Creating the map centred over UOM with default zoom level 15
+function initMap() {
+    const ict = new google.maps.LatLng(35.901810199007215, 14.485197413626922);
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: ict,
+        zoom: 15,
+    });    
+    //setting the bounds of UOM. Bounds [zoom][coord.x][coord.y]
+    //bounds are generated from map tiler folder structure when tiles are created
+    const bounds = {
+        19: [
+            [283239, 283240],
+            [206056, 206057],
+        ],
+        20: [
+            [566478, 566480],
+            [412112, 412115],
+        ],
+        21: [
+            [1132957, 1132960],
+            [824225, 824231],
+        ],
+    };
+
+    //https://developers.google.com/maps/documentation/javascript/examples/maptype-image-overlay#maps_maptype_image_overlay-javascript
+    //creating the imageMapType for the image to be overlayed over the map
+    const blkA = new google.maps.ImageMapType({
+        getTileUrl: function (coord, zoom) {
+            //if statement to not render the image if outside the bounds or outside the max/min zoom levels
+            if (
+                zoom < 19 ||
+                zoom > 21 ||
+                bounds[zoom][0][0] > coord.x ||
+                coord.x > bounds[zoom][0][1] ||
+                bounds[zoom][1][0] > coord.y ||
+                coord.y > bounds[zoom][1][1]
+            ) {
+                return "";
+            }
+            //returning the required tiles by requesting the specific png images
+            //link is according to the port being used 
+            return [
+                "https://localhost:44346/StaticFile/images/ICTTiles/BlockA/",
+                zoom,
+                "/",
+                coord.x,
+                "/",
+                coord.y,
+                ".png",
+            ].join("");
+        },
+        tileSize: new google.maps.Size(256, 256),
+    });
+    const blkB = new google.maps.ImageMapType({
+        getTileUrl: function (coord, zoom) {
+            //if statement to not render the image if outside the bounds or outside the max/min zoom levels
+            if (
+                zoom < 19 ||
+                zoom > 21 ||
+                bounds[zoom][0][0] > coord.x ||
+                coord.x > bounds[zoom][0][1] ||
+                bounds[zoom][1][0] > coord.y ||
+                coord.y > bounds[zoom][1][1]
+            ) {
+                return "";
+            }
+            //returning the required tiles by requesting the specific png images
+            //link is according to the port being used 
+            return [
+                "https://localhost:44346/StaticFile/images/ICTTiles/BlockB/",
+                zoom,
+                "/",
+                coord.x,
+                "/",
+                coord.y,
+                ".png",
+            ].join("");
+        },
+        tileSize: new google.maps.Size(256, 256),
+    });
+    map.overlayMapTypes.push(blkA);
+    map.overlayMapTypes.push(blkB);
+}
---
+++ BuddyAPI/wwwroot/Controls/js/infoWindow.js
@@ -0,0 +1,59 @@
+﻿
+//variable to hold the optional info window displaying zoom level, latlng, world, pixel and tile coordinates
+function initMap() {
+    const ict = new google.maps.LatLng(35.901810199007215, 14.485197413626922);
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: ict,
+        zoom: 15,
+    });
+    const coordInfoWindow = new google.maps.InfoWindow();
+    coordInfoWindow.setContent(createInfoWindowContent(ict, map.getZoom()));
+    coordInfoWindow.setPosition(ict);
+    coordInfoWindow.open(map);
+    //event listener that fires when zoom level changes
+    map.addListener("zoom_changed", () => {
+        coordInfoWindow.setContent(
+            createInfoWindowContent(ict, map.getZoom())
+        );
+        coordInfoWindow.open(map);
+    });
+}
+
+
+const TILE_SIZE = 256;
+
+//https://developers.google.com/maps/documentation/javascript/examples/map-coordinates#all
+//funtion to create info window
+function createInfoWindowContent(latLng, zoom) {
+    const scale = 1 << zoom;
+    const worldCoordinate = project(latLng);
+    const pixelCoordinate = new google.maps.Point(
+        Math.floor(worldCoordinate.x * scale),
+        Math.floor(worldCoordinate.y * scale)
+    );
+    const tileCoordinate = new google.maps.Point(
+        Math.floor((worldCoordinate.x * scale) / TILE_SIZE),
+        Math.floor((worldCoordinate.y * scale) / TILE_SIZE)
+    );
+    return [
+        "University of Malta, MT",
+        "LatLng: " + latLng,
+        "Zoom level: " + zoom,
+        "World Coordinate: " + worldCoordinate,
+        "Pixel Coordinate: " + pixelCoordinate,
+        "Tile Coordinate: " + tileCoordinate,
+    ].join("<br>");
+}
+
+//https://developers.google.com/maps/documentation/javascript/examples/map-coordinates#all
+//function to return world coordinates when supplying latlng
+function project(latLng) {
+    let siny = Math.sin((latLng.lat() * Math.PI) / 180);
+    // Truncating to 0.9999 effectively limits latitude to 89.189. This is
+    // about a third of a tile past the edge of the world tile.
+    siny = Math.min(Math.max(siny, -0.9999), 0.9999);
+    return new google.maps.Point(
+        TILE_SIZE * (0.5 + latLng.lng() / 360),
+        TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI))
+    );
+}
