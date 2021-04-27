06dc3db3d6c5618f830a9a6f9f61b3d5e0e639a4
LiamCurmideGray <45282827+LiamCurmideGray@users.noreply.github.com>
2021-04-27 14:06:39 +0200

    Working Roles Calls
    Created html page to have a landing page for the data
    Used javascript files to call the data to to db through the API calls.
    
    All checks done and in working order:
    getAllRoles
    getRolebyId
    PostNewRole
    UpdateRole
    deleteRole

--- BuddyAPI/Startup.cs
+++ BuddyAPI/Startup.cs
@@ -48,6 +48,7 @@ namespace BuddyAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BuddyAPI v1"));
             }
 
+            //app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseHttpsRedirection();
